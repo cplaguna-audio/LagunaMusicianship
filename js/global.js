1a8b77a4b690067309d24d4bdbae26cfa2e940b5
Christopher Laguna <cplaguna37@gmail.com>
2017-03-09 19:56:07 -0800

    moved common header/fooder code into shared files

--- js/global.js
+++ js/global.js
@@ -7,19 +7,29 @@ define([
    * Determine the size of the video: the entire screen minus a multiple of
    * the height of the header.
    */
-  var aspect_ratio = 768 / 480; /* width over height */
-  $(document).ready(function() {
+  function CalculateVideoSize() {
     var videos = $(".video_iframe");
     for(var idx = 0; idx < videos.length; idx++) {
-      var headers = $(".lm-header");
+      var headers = $("#header_common");
       var header_height = 0;
       if(headers.length > 0) {
         header_height = $(headers[0]).height();
+        console.log("screen height: " + screen.height);
+        console.log("header height: " + header_height);
       }
       var height = (screen.height * 0.9) - (3 * header_height);
       videos[idx].style.height = "" + height + "px";
       videos[idx].style.width = "" + (height * aspect_ratio) + "px";
     }
+  }
+
+  var aspect_ratio = 768 / 480; /* width over height */
+  $(document).ready(function() {
+
+    /* Load the common html. */
+    $("#header_common").load("html/common/header.html", null, CalculateVideoSize);
+    $("#footer_common").load("html/common/footer.html");
+
   });
 
   /* Public variables go here. */

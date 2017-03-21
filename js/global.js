e56d45f63b0dc2a9be10109c232fa3303b001472
Christopher Laguna <cplaguna37@gmail.com>
2017-03-21 15:51:22 -0700

    Added skelleton for site navigation.

--- js/global.js
+++ js/global.js
@@ -9,26 +9,46 @@ define([
    */
   function CalculateVideoSize() {
     var videos = $(".video_iframe");
+    var headers = $("#header_common");
+    var footers = $("#footer_common");
+
+    var header_height = 0;
+    if(headers.length > 0) {
+      header_height = $(headers[0]).height();
+    }
+
+    var footer_height = 0;
+    if(footers.length > 0) {
+      footer_height = $(footers[0]).height();
+    }
+
     for(var idx = 0; idx < videos.length; idx++) {
-      var headers = $("#header_common");
-      var header_height = 0;
-      if(headers.length > 0) {
-        header_height = $(headers[0]).height();
-        console.log("screen height: " + screen.height);
-        console.log("header height: " + header_height);
-      }
-      var height = (screen.height * 0.9) - (3 * header_height);
+      var height = (screen.height * 0.88) - (3 * header_height);
       videos[idx].style.height = "" + height + "px";
       videos[idx].style.width = "" + (height * aspect_ratio) + "px";
     }
+
+    var contents = $(".lm_content");
+    if(contents.length > 0) {
+      var content = contents[0];
+      var content_height = screen.height - header_height - footer_height;
+      content.style.minHeight = "" + content_height + "px"; 
+    }
+
   }
 
   var aspect_ratio = 768 / 480; /* width over height */
   $(document).ready(function() {
 
     /* Load the common html. */
-    $("#header_common").load("html/common/header.html", null, CalculateVideoSize);
-    $("#footer_common").load("html/common/footer.html");
+    $.when(
+      $.get("html/common/header.html"),
+      $.get("html/common/footer.html")
+    ).then(function(header_response, footer_response) {
+      $("#header_common").html(header_response[0]);
+      $("#footer_common").html(footer_response[0]);
+      CalculateVideoSize();
+    });
 
   });
 
